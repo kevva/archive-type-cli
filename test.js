55caf26344c26ce5cc4fd791c2910783182e8327
Kevin Martensson <kevinmartensson@gmail.com>
2016-01-06 18:48:20 +0100

    Add more tests

--- test.js
+++ test.js
@@ -5,3 +5,10 @@ test('show help screen', async t => {
 	const ret = await execa('./cli.js', ['--help']);
 	t.regexTest(/Detect the archive type of a Buffer/, ret.stdout);
 });
+
+test('get archive type', async t => {
+	const tar = await execa('./cli.js', ['fixtures/test.tar']);
+	const zip = await execa('./cli.js', ['fixtures/test.zip']);
+	t.is('tar', tar.stdout);
+	t.is('zip', zip.stdout);
+});
